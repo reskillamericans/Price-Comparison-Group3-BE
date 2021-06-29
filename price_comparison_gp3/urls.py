9b47d54ce4965d6ce156afba8ab783ebf2358351
XanderSheppard <xandersheppard@gmail.com>
2021-06-29 09:46:02 -0400

    API Fetching for amazon and ebay

--- price_comparison_gp3/urls.py
+++ price_comparison_gp3/urls.py
@@ -15,9 +15,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from .views import index, index2
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('price_comparison.urls')),
+    path('', index),
+    path('', index2),
+
 ]
